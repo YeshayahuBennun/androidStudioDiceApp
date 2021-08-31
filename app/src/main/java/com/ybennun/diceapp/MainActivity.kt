c677bb3c1adee3c69cbb9fd195aacfd61380cabb
YeshayahuBennun <ybennun@yahoo.com>
2021-08-31 17:56:56 +0300

    commit#2 - Create Dice App Functionality(Variables,            findViewById)

--- app/src/main/java/com/ybennun/diceapp/MainActivity.kt
+++ app/src/main/java/com/ybennun/diceapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.ybennun.diceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton = findViewById<Button>(R.id.btn_roll_dice)
+
+        rollButton.setOnClickListener{
+            rollDice()
+        }
+    }
+
+    fun rollDice(){
+
+        val randomNumber =(1..6).random()
+        val randomNumberTxt = findViewById<TextView>(R.id.text_view_random_number)
+        randomNumberTxt.text = randomNumber.toString()
     }
 }
